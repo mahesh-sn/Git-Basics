6b272350b0edaadf52f38a65c5d24c609dbd6090
mahesh-sn <maheshsn705@gmail.com>
2024-08-29 23:41:23 +0530

    Bug fix

--- src/base/FirstApp.java
+++ src/base/FirstApp.java
@@ -3,6 +3,7 @@ package base;
 public class FirstApp {
 	public static void main(String[] args) {
 		//First feature added
-		System.out.println("first featiure");
+		System.out.println("first feature added");
+		
 	}
 }
